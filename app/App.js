b458eed4bbacd699e9fd405b6b77f5e92ec07cad
williamjosh <williamjosh121@gmail.com>
2019-08-17 20:13:24 +0400

    Initial commit

--- app/App.js
+++
@@ -1,8 +0,0 @@
-import App from './src/index';
-require('react-native-browser-polyfill');
-
-if (global && !global.self && Platform.OS === 'android') {
-  global.self = global;
-}
-
-export default App;
--- app/src/apollo.js
+++
@@ -1,9 +0,0 @@
-import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-
-// add ip address of your machine
-export const client = new ApolloClient({
-  link: new HttpLink({ uri: 'http://192.168.100.23:5000/graphql' }),
-  cache: new InMemoryCache()
-})
